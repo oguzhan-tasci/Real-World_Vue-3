97c9c9778667011554dfc5c24b2ff11f3dff4106
oyeProjects <oyeprojects@hotmail.com>
2022-07-24 14:02:39 +0300

    routers unsterstood

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!--
+        When we navigate to that path, because of this route object here, Vue Router knows which component to load up.
+        In this case, we're loading up the home component, but where do we put it ?
+        That's where the 'router-view' comes in.
+        This functions as a placeholder that is replaced by the route's actual component.
+       -->
+
+      <!-- 
+        Our to-do list for this section includes :
+        renaming Home.vue to EventList.vue
+        Then customize the route for EventList,
+        Update the 'About.vue',
+        And reconfigure the About route.
+      -->
+
+      <router-link to="/">Events</router-link> |
+      <router-link to="/aboutt">About</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
+import EventList from '../views/EventList.vue'
+import About from '../views/About.vue'
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'EventList',
+    component: EventList
   },
   {
-    path: '/about',
+    path: '/aboutt',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+
+    // This route is configured a bit differently.
+    // What's going on here ?
+    // As the comme tells us, this is for route-level code splitting.
+    // In other words, this is a performance optimization.
+    // When were walking about client-side routing, I mentioned how we're sent the inital page,index.html, including the rest of the app.
+    // Some of you may have noticed that there is an asterisk here 
+    // because there are situations like this where we're doing route-level code splitting, 
+    // lazy loading components, so we're not actually sending over the entire application code on the first load of the app.
+
+    // If your app is quite large,
+    // it might actualy not make much sense from a performance standpoint to send your user all of your application code,
+    // every page, every view, even tough they might not need it or might not need it yer
+    // component: () =>
+    //   import( /* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
